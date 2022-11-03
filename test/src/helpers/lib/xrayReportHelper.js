cb43fdad31cbb19a79b8492482115a2eed8d7538
Javier del Olmo <javier.delolmo@iovlabs.org>
2022-11-03 13:48:53 +0000

    Exception handling improvement

--- test/src/helpers/lib/xrayReportHelper.js
+++ test/src/helpers/lib/xrayReportHelper.js
@@ -6,16 +6,18 @@ async function submitCucumberTestResults(resultsFile, config) {
     
     console.log('Uploading reports to Jira Xray...');
     const xrayClient = new XrayCloudClient();
-
+    const baseURL = 'https://rsklabs.atlassian.net/browse/';
     return xrayClient.submitResultsMultipart(resultsFile, multipartConfig)
     .then( response => { 
-        console.log('Test Execution created \'' + response.key + '\'');
+        console.log('Test Execution created: \'' + baseURL + response.key + '\'');
         // DEBUG return new XrayCloudResponseV2(response);
     }).catch( error => { 
-        if (error.body.error !== undefined)
+        if (error.body !== undefined)
             throw new Error(error.body.error);
+        else if (error._response !== undefined)
+            throw new Error(error._response);
         else
-            throw new Error(error.message);
+            return new XrayCloudResponseV2(error);
     });
 }
 
@@ -30,11 +32,12 @@ async function downloadCucumberFeatures(config) {
         console.log('Features from \'' + response._response.config.url +  '\' created on \'' + cucumberConfig.featuresPath +'\'');
         // DEBUG return new XrayCloudResponseV2(response._response);
     }).catch( function(error) {
-        if (error._response !== undefined && error._response.status == 400 ) {
+        if (error._response !== undefined && error._response.status == 400 )
             throw new Error('Invalid keys/filter id or either filter is not public!');   
-        } else {
-            throw new Error(error.body.error);   
-        }
+        else if (error.body !== undefined)
+            throw new Error(error.body.error);
+        else
+            return new XrayCloudResponseV2(error);
     });        
 }
 
@@ -77,8 +80,10 @@ async function uploadCucumberFeatures(config) {
     }).catch( function(error) {
         if (error.response !== undefined)
             throw new XrayErrorResponse(error.response);
+        else if (error.body !== undefined)
+            throw new Error(error.body.error);
         else
-            throw new Error(error.message || error.body.error);
+            throw new Error(error.message);  
     });
 }
 
